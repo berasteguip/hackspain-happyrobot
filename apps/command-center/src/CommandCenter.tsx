c340e960f5b127dcd659c917ba253c2916f3f965
allanbees <allandbc1202@gmail.com>
2026-09-20 07:51:55 +0200

    Ajusta la entrada a 1.7 segundos

--- apps/command-center/src/CommandCenter.tsx
+++ apps/command-center/src/CommandCenter.tsx
@@ -5,7 +5,7 @@ import { HappyRobotLogo, HappyRobotSymbol } from './HappyRobot'
 import { Wordmark } from './Logo'
 
 /** Lo que dura la entradilla. El velo la acompaña desde el CSS: --intro. */
-const INTRO_MS = 1000
+const INTRO_MS = 1700
 import { fetchFirmsSpain } from './firms'
 import { DEFAULT_SCENARIO_ID, SCENARIOS, scenarioById } from './scenarios'
 import { anchorScenario } from './scenario'
